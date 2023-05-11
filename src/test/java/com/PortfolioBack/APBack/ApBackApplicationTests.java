327daeb3f47cd79607177c78d3a2debe74af75b1
anasolfran@hotmail.com <35575747+AnaSolF@users.noreply.github.com>
2023-05-11 15:18:01 -0300

    Archivos iniciales

---
+++ src/test/java/com/PortfolioBack/APBack/ApBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PortfolioBack.APBack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
