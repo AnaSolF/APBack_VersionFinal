6004f36aaffe91e2ea4dabd9d53a9eedb03700d9
anasolfran@hotmail.com <35575747+AnaSolF@users.noreply.github.com>
2023-05-15 19:57:27 -0300

    Configuracion de cors.

--- src/main/java/com/PortfolioBack/APBack/Controllers/CorsConfig.java
+++ src/main/java/com/PortfolioBack/APBack/Controllers/CorsConfig.java
@@ -1,23 +1,17 @@
 package com.PortfolioBack.APBack.Controllers;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig {
-
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                    .allowedOrigins("*")
-                    .allowedMethods("GET", "POST", "PUT", "DELETE")
-                    .allowedHeaders("*");
-            }
-        };
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+            .allowedOrigins("https://proyecto-argentina-progr-b5a50.web.app")
+            .allowedMethods("GET", "POST", "PUT", "DELETE")
+            .allowedHeaders("Authorization", "Content-Type")
+            .allowCredentials(true);
     }
 }
